0315fc1eca35b0236370d24097c20a31a6ec682b
johannesmadest <johannesmadest@gmail.com>
2017-05-28 21:51:53 +0200

    weitere änderungen

---
+++ gui 0.1/src/AusfEinzel.java
@@ -0,0 +1,64 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.FlowLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+
+public class AusfEinzel extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AusfEinzel frame = new AusfEinzel();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AusfEinzel() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 450);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblBitteGebenSie = new JLabel("Bitte geben Sie Ihre Daten ein!");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(292)
+					.addComponent(lblBitteGebenSie, GroupLayout.PREFERRED_SIZE, 180, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(302, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblBitteGebenSie)
+					.addContainerGap(376, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
--- gui 0.1/src/Ausleihfenster.java
+++ gui 0.1/src/Ausleihfenster.java
@@ -17,6 +17,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class Ausleihfenster extends JFrame {
 
@@ -47,16 +48,11 @@ public class Ausleihfenster extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 			
 		JLabel lblTitel1 = new JLabel("Wilkommen zum Buchverleih");
 		lblTitel1.setBackground(Color.GRAY);
-		lblTitel1.setBounds(266, 12, 200, 33);
-		contentPane.add(lblTitel1);
 		
 		JLabel lblText1 = new JLabel("Für wen wollen sie Bücher ausleihen?");
-		lblText1.setBounds(266, 96, 267, 23);
-		contentPane.add(lblText1);
 		
 		JButton btnback1 = new JButton("Zurück");
 		btnback1.addActionListener(new ActionListener() {
@@ -66,21 +62,61 @@ public class Ausleihfenster extends JFrame {
 			hauptfenster.main(null);
 			}
 		});
-		btnback1.setBounds(10, 12, 89, 23);
-		contentPane.add(btnback1);
 		
 		JLabel lblU = new JLabel("Unterstufe");
+		lblU.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				AusfUnter ausfu1=new AusfUnter();
+				dispose();
+				ausfu1.setVisible(true);
+			}
+		});
 		Image img1 = new ImageIcon(this.getClass().getResource("/path3776.png")).getImage();
 		lblU.setIcon(new ImageIcon(img1));
-		lblU.setBounds(62, 263, 104, 54);
-		contentPane.add(lblU);
 		
 		JLabel lblO = new JLabel("Oberstufe");
-		lblO.setBounds(372, 283, 46, 14);
-		contentPane.add(lblO);
 		
 		JLabel lblE = new JLabel("Einzeln");
-		lblE.setBounds(636, 283, 46, 14);
-		contentPane.add(lblE);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnback1, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 207, Short.MAX_VALUE)
+					.addComponent(lblTitel1, GroupLayout.PREFERRED_SIZE, 188, GroupLayout.PREFERRED_SIZE)
+					.addGap(280))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addGap(233)
+					.addComponent(lblText1, GroupLayout.DEFAULT_SIZE, 435, Short.MAX_VALUE)
+					.addGap(177))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(63)
+					.addComponent(lblU, GroupLayout.PREFERRED_SIZE, 99, Short.MAX_VALUE)
+					.addGap(141)
+					.addComponent(lblO, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)
+					.addGap(111)
+					.addComponent(lblE, GroupLayout.PREFERRED_SIZE, 113, GroupLayout.PREFERRED_SIZE)
+					.addGap(132))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(6)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnback1)
+						.addComponent(lblTitel1, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE))
+					.addGap(42)
+					.addComponent(lblText1, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+					.addGap(130)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblU)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+							.addComponent(lblO, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+							.addComponent(lblE, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)))
+					.addGap(113))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 }
