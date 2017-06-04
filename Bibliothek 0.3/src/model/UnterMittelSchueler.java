55c453510dd2ebb2b4e9295692f121620b225812
MFritzN <mfritzn@gmail.com>
2017-06-04 17:44:03 +0200

    Controller angefangen, Bibliothek 0.3 erstellt, überprüfung nach erststart über erststart.dat implementiert

---
+++ Bibliothek 0.3/src/model/UnterMittelSchueler.java
@@ -0,0 +1,16 @@
+package model;
+
+public class UnterMittelSchueler {
+	String vorname;
+	String nachname;
+	String gebDat;
+	UnterMittelStufe klasse;
+	
+	
+	public UnterMittelSchueler (String vorname, String nachname, String geburtsDatum, UnterMittelStufe klasse) {
+		this.vorname = vorname;
+		this.nachname = nachname;
+		this.gebDat = geburtsDatum;
+		this.klasse = klasse;
+	}
+}
